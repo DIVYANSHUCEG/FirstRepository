a2d747bd663409b4fe949bdb62fc1ececd459539
DIVYANSHUCEG <divyanshuceg@gmail.com>
2017-01-08 20:54:31 +0530

    Create CalcExample.java

---
+++ CalcExample.java
@@ -0,0 +1,15 @@
+# FirstRepository
+public class CalcExample{
+	static int add(int a,int b){
+		return 0;
+	}
+	static int sub(int a,int b){
+		return 0;
+	}
+	static int multiply(int a,int b){
+		return 0;
+	}
+	static int divide(int a,int b){
+		return 0;
+	}
+}
